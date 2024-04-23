b23edaa45b61b135de3361fcedb8b4a1645367f9
Javascriptbabe <javascriptbabe@gmail.com>
2024-04-23 16:42:22 -0700

    added active classes

--- JAVASCRIPT/nav.js
+++ JAVASCRIPT/nav.js
@@ -4,22 +4,26 @@ const arrow2 = document.querySelector('.arrow2');
 const arrow3 = document.querySelector('.arrow3');
 const expertiseFloat = document.querySelector('.expertise-float');
 const ourTeamFloat = document.querySelector('.our-team-float');
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach((parameter) => {
+  if (parameter.href.includes(`${activePage}`)) {
+    parameter.classList.add('active')
+  }
+});
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('nav-open');
-})
-
-
-
-// nav.addEventListener('click', () => {
-//   nav.style.display = 'none';
-// })
+});
 
 arrow3.addEventListener('click', () => {
   expertiseFloat.classList.toggle('expertise-open');
   arrow3.classList.toggle('add');
 });
+
 arrow2.addEventListener('click', () => {
   ourTeamFloat.classList.toggle('our-team-float-open');
   arrow2.classList.toggle('add');
 });
+
